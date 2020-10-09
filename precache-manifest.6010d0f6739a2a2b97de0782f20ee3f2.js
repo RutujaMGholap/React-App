096d37ba59986bb75964648d7bd63f30a98a5778
RutujaMGholap <rutujamgholap99@gmail.com>
2020-10-09 15:33:45 +0530

    Updates

---
+++ precache-manifest.6010d0f6739a2a2b97de0782f20ee3f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d5782a5d418cca44fc6a75dc9ad2b2",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "28598e8375902a6b23b6",
+    "url": "/React-App/static/css/2.bb518e5e.chunk.css"
+  },
+  {
+    "revision": "af3217155fb2415cc469",
+    "url": "/React-App/static/css/main.e03eb87c.chunk.css"
+  },
+  {
+    "revision": "28598e8375902a6b23b6",
+    "url": "/React-App/static/js/2.21f6ee34.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/React-App/static/js/2.21f6ee34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3217155fb2415cc469",
+    "url": "/React-App/static/js/main.bd3d5c80.chunk.js"
+  },
+  {
+    "revision": "94511e1b3a56b9cab451",
+    "url": "/React-App/static/js/runtime-main.5b399332.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
